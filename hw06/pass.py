dc55261a555eab2cfd05e51d5be89f26f8a4f8e4
Ho Yin Ho <hoyinho999@gmail.com>
2016-03-31 11:29:17 -0400

    completed first task

--- hw06/pass.py
+++ hw06/pass.py
@@ -8,7 +8,9 @@ def passChecker(pw):
     numbers = "1234567890"
     checker = [1 if x in upperC else 2 if x in lowerC else  3 if x in numbers else 0 for x in pw]
     return (1 in checker and 2 in checker and 3 in checker and not 0 in checker)
- 
+
+
+print "Checking for valid passwords"
 print "password is password returns " + str(passChecker("password"))
 print "password is PASSWORD returns " + str(passChecker("PASSWORD"))
 print "password is PassWord returns " +  str(passChecker("PassWord"))
